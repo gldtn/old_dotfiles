939bcb5b9878805b2e4af80b11e1501582abbedb
gldtn <g@gldtn.io>
2023-11-15 19:05:18 -0500

    toggle-theme add interactive mode

--- .config/alacritty/toggle-theme.py
+++ .config/alacritty/toggle-theme.py
@@ -22,19 +22,19 @@ COLORS = {
 script_name = os.path.basename(sys.argv[0]).replace('.py', '')
 usage = f"{COLORS['RED']}Usage{COLORS['RESET']}: {script_name} [{COLORS['CYAN']}OPTION{COLORS['RESET']}]\n"
 
-# Get the user's home directory
+# Directories
 user_home = os.path.expanduser('~')
 theme_directory = os.path.join(user_home, '.config/alacritty/themes')
 theme_loader_path = os.path.join(user_home, '.config/alacritty/theme_loader.toml')
 alacritty_config = os.path.join(user_home, '.config/alacritty/alacritty.toml')
 
-# Function to switch themes by index or name
+# Switch themes by index or name
 def toggle_theme(theme_input):
     if not theme_input:
-        print("Please provide a theme index, name, or use the '-r' or '--random' option.")
+        print("Please provide a theme index or name.")
         return
 
-    if theme_input.isdigit():  # If input is a number, treat as index
+    if theme_input.isdigit():  # number, treat as index
         index = int(theme_input)
         if 1 <= index <= len(themes):
             theme_name = themes[index - 1]
@@ -58,17 +58,17 @@ def apply_theme(theme_name):
     touch_file(alacritty_config)
     print(f"Theme '{theme_name}' applied successfully!")
 
-# Function to list available themes with index
+# List indexed themes
 def list_themes():
     global themes
     themes = [f.replace('.toml', '') for f in os.listdir(theme_directory) if f.endswith('.toml')]
-    themes.sort()  # Sort themes alphabetically
+    themes.sort()  # sort alphabetically
     print("Available themes:\n")
     for i, theme in enumerate(themes, start=1):
         print(f"{i}. {theme}")
-    print("\nSelect by index, name, or use the '-r' or '--random' option; i.e > toggle-theme 1")
+    print("\nSelect by index or name; i.e > toggle-theme 1")
 
-# Function to update alacritty config file
+# Update alacritty config file
 def touch_file(file_path):
     with open(file_path, 'a'):
         os.utime(file_path, None)
@@ -76,13 +76,14 @@ def touch_file(file_path):
 def underline_text(text):
     return '\033[4m' + text + '\033[0m'
 
-# Function to display available options
+# Options
 def display_help():
     options = [
-        f"{COLORS['CYAN']}-r, --random{COLORS['RESET']}  : Apply a random theme",
-        f"{COLORS['CYAN']}-l, --list{COLORS['RESET']}    : List available themes",
-        f"{COLORS['CYAN']}-h, --help{COLORS['RESET']}    : Show available command options",
-        f"{COLORS['CYAN']}index/name{COLORS['RESET']}    : Select a theme by index or name",
+        f"{COLORS['CYAN']}-r, --random{COLORS['RESET']}         : Apply a random theme",
+        f"{COLORS['CYAN']}-l, --list{COLORS['RESET']}           : List available themes",
+        f"{COLORS['CYAN']}-i, --interactive{COLORS['RESET']}    : Enter interactive mode",
+        f"{COLORS['CYAN']}-h, --help{COLORS['RESET']}           : Show available command options",
+        f"{COLORS['CYAN']}index/name{COLORS['RESET']}           : Select a theme by index or name",
     ]
 
     print(f"A simple Alacritty theme switcher written in Python\n")
@@ -93,14 +94,14 @@ def display_help():
 
 # Check arguments to switch, list themes, or display help
 if len(sys.argv) < 2:
-    print("Please provide a theme index, name, or use the '-r' or '--random' option.\n")
+    print("Please provide a theme index or name.\n")
     print(f"{COLORS['CYAN']}{underline_text(usage)}{COLORS['RESET']}")
     print(f"For more information, try '--help'.")
 else:
     theme_input = sys.argv[1]
-    if theme_input not in ['-h', '--help', '-l', '--list']:
+    if theme_input not in ['-h', '--help', '-l', '--list', '-i', '--interactive']:
         themes = [f.replace('.toml', '') for f in os.listdir(theme_directory) if f.endswith('.toml')]
-        themes.sort()  # Sort themes alphabetically
+        themes.sort()  # sort alphabetically
 
         if not themes:
             print("No themes available. Please add themes to /themes/ dir!")
@@ -109,9 +110,23 @@ else:
                 theme_name = random.choice(themes)
                 apply_theme(theme_name)
                 print(f"Random theme '{theme_name}' applied successfully!")
+            elif theme_input == '-i' or theme_input == '--interactive':
+                list_themes()
+                while True:
+                    user_input = input("Enter the theme index or name (type 'q' to quit): ")
+                    if user_input.lower() == 'q':
+                        break
+                    toggle_theme(user_input)
             else:
                 toggle_theme(theme_input)
     elif theme_input in ['-h', '--help']:
         display_help()
     elif theme_input in ['-l', '--list']:
         list_themes()
+    elif theme_input in ['-i', '--interactive']:
+        list_themes()
+        while True:
+            user_input = input("Enter the theme index or name (type 'q' to quit): ")
+            if user_input.lower() == 'q':
+                break
+            toggle_theme(user_input)
