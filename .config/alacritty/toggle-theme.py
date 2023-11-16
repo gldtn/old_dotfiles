9c930efdf0a4b79145a1fd6f5c51b3c70b3f5adb
gldtn <g@gldtn.io>
2023-11-15 23:23:53 -0500

    theme_loader.toml no longer

--- .config/alacritty/toggle-theme.py
+++ .config/alacritty/toggle-theme.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-### Toggle-theme v1.0 by gldtn
-## You may need to create an empty theme_loader.toml file to alacritty folder
-## https://github.com/gldtn/dotfiles/tree/main/.config/alacritty
+###
+# Toggle-theme v1.1 by gldtn
+# https://github.com/gldtn/dotfiles/tree/main/.config/alacritty
+# Note: import must be present in your alacritty config:
+# import = [""]
+###
 
 import os
-import shutil
+import re
 import sys
+import shutil
 import random
 
 # ANSI color
@@ -18,6 +22,10 @@ COLORS = {
     'RESET': '\033[0m',
 }
 
+# Function to underline text
+def underline_text(text):
+    return '\033[4m' + text + '\033[0m'
+
 # Global variables
 script_name = os.path.basename(sys.argv[0]).replace('.py', '')
 usage = f"{COLORS['RED']}Usage{COLORS['RESET']}: {script_name} [{COLORS['CYAN']}OPTION{COLORS['RESET']}]\n"
@@ -25,38 +33,46 @@ usage = f"{COLORS['RED']}Usage{COLORS['RESET']}: {script_name} [{COLORS['CYAN']}
 # Directories
 user_home = os.path.expanduser('~')
 theme_directory = os.path.join(user_home, '.config/alacritty/themes')
-theme_loader_path = os.path.join(user_home, '.config/alacritty/theme_loader.toml')
 alacritty_config = os.path.join(user_home, '.config/alacritty/alacritty.toml')
 
 # Switch themes by index or name
 def toggle_theme(theme_input):
     if not theme_input:
         print("Please provide a theme index or name.")
-        return
+        return None
+
+    applied_theme = None
 
     if theme_input.isdigit():  # number, treat as index
         index = int(theme_input)
         if 1 <= index <= len(themes):
-            theme_name = themes[index - 1]
-            apply_theme(theme_name)
-        else:
-            print(f"{COLORS['RED']}Error{COLORS['RESET']}: Invalid index. Please provide a valid theme index.")
+            applied_theme = themes[index - 1]
     elif theme_input == '-r' or theme_input == '--random':
-        theme_name = random.choice(themes)
-        apply_theme(theme_name)
-        print(f"Random theme '{theme_name}' applied successfully!")
-    else:  # Treat input as theme name
-        if theme_input in themes:
-            apply_theme(theme_input)
-        else:
-            print(f"{COLORS['RED']}Error{COLORS['RESET']}: Theme '{COLORS['BOLD']}{theme_input}{COLORS['RESET']}' not found. Please provide a valid theme name.")
+        applied_theme = random.choice(themes)
+    elif theme_input in themes:  # Treat input as theme name
+        applied_theme = theme_input
+
+    if applied_theme is not None:
+        apply_theme(applied_theme)
+        return applied_theme
+    else:
+        print(f"{COLORS['RED']}Error{COLORS['RESET']}: Invalid input. Please provide a valid theme index or name.")
+        return None
 
 # Apply the selected theme
 def apply_theme(theme_name):
     theme_config = os.path.join(theme_directory, f'{theme_name}.toml')
-    shutil.copy(theme_config, theme_loader_path)
-    touch_file(alacritty_config)
-    print(f"Theme '{theme_name}' applied successfully!")
+    import_line = f'import = ["{os.path.join(theme_directory, f"{theme_name}.toml")}"]'
+
+    with open(alacritty_config, 'r') as f:
+        config_content = f.read()
+
+    config_content = re.sub(r'import = \[".*"\]', import_line, config_content)
+
+    with open(alacritty_config, 'w') as f:
+        f.write(config_content)
+
+    return theme_name
 
 # List indexed themes
 def list_themes():
@@ -67,14 +83,6 @@ def list_themes():
     for i, theme in enumerate(themes, start=1):
         print(f"{i}. {theme}")
     print("\nSelect by index or name; i.e > toggle-theme 1")
-
-# Update alacritty config file
-def touch_file(file_path):
-    with open(file_path, 'a'):
-        os.utime(file_path, None)
-
-def underline_text(text):
-    return '\033[4m' + text + '\033[0m'
 
 # Options
 def display_help():
@@ -101,24 +109,29 @@ else:
     theme_input = sys.argv[1]
     if theme_input not in ['-h', '--help', '-l', '--list', '-i', '--interactive']:
         themes = [f.replace('.toml', '') for f in os.listdir(theme_directory) if f.endswith('.toml')]
-        themes.sort()  # sort alphabetically
+        themes.sort()  # sort themes alphabetically
 
         if not themes:
             print("No themes available. Please add themes to /themes/ dir!")
         else:
             if theme_input == '-r' or theme_input == '--random':
                 theme_name = random.choice(themes)
-                apply_theme(theme_name)
-                print(f"Random theme '{theme_name}' applied successfully!")
+                applied_theme = toggle_theme(theme_name)
+                if applied_theme:
+                    print(f"Theme '{applied_theme}' applied successfully!")
             elif theme_input == '-i' or theme_input == '--interactive':
                 list_themes()
                 while True:
                     user_input = input("Enter the theme index or name (type 'q' to quit): ")
                     if user_input.lower() == 'q':
                         break
-                    toggle_theme(user_input)
+                    applied_theme = toggle_theme(user_input)
+                    if applied_theme:
+                        print(f"Theme '{applied_theme}' applied successfully!")
             else:
-                toggle_theme(theme_input)
+                applied_theme = toggle_theme(theme_input)
+                if applied_theme:
+                    print(f"Theme '{applied_theme}' applied successfully!")
     elif theme_input in ['-h', '--help']:
         display_help()
     elif theme_input in ['-l', '--list']:
@@ -129,4 +142,6 @@ else:
             user_input = input("Enter the theme index or name (type 'q' to quit): ")
             if user_input.lower() == 'q':
                 break
-            toggle_theme(user_input)
+            applied_theme = toggle_theme(user_input)
+            if applied_theme:
+                print(f"Theme '{applied_theme}' applied successfully!")
