ad053af1741bb92be02f4f58484400d89e2c06cb
gldtn <g@gldtn.io>
2024-04-30 20:55:21 -0400

    refactor colorscheme util

--- .config/nvim/lua/utils/colorscheme.lua
+++ .config/nvim/lua/utils/colorscheme.lua
@@ -1,8 +1,9 @@
--- Setup color variables
+-- Set color variables
 local color = require("tokyonight.colors").setup()
 local darken = require("tokyonight.util").darken
-local darken_bg = darken(color.comment, 0.3)
-local icon = { fg = color.blue, bg = darken(color.comment, 0.3) }
+local default_bg = darken(color.comment, 0.3)
+local default = { fg = color.blue, bg = darken(color.comment, 0.3) }
+
 local hl = function(group, attributes)
   vim.api.nvim_set_hl(0, group, attributes)
 end
@@ -14,17 +15,17 @@ hl("CmpItemAbbrMatch", { fg = color.blue, bg = "NONE", bold = true })
 hl("CmpItemAbbrMatchFuzzy", { fg = color.blue, bg = "NONE", bold = true })
 hl("CmpItemAbbrDeprecated", { fg = darken(color.fg, 0.5), bg = "NONE", strikethrough = true })
 
--- Match and set attributes for highlight groups starting with "CmpItemKind"
+-- Set defaults to "CmpItemKind*"
 for _, group in ipairs(vim.fn.getcompletion("CmpItemKind*", "highlight")) do
-  hl(group, icon)
+  hl(group, default)
 end
 
--- Set item kind highlights
-hl("CmpItemKindText", { fg = color.green, bg = darken_bg })
-hl("CmpItemKindSnippet", { fg = color.yellow, bg = darken_bg })
-hl("CmpItemKindCopilot", { fg = color.green1, bg = darken_bg })
-hl("CmpItemKindKeyword", { fg = color.purple, bg = darken_bg })
-hl("CmpItemKindOperator", { fg = color.blue5, bg = darken_bg })
-hl("CmpItemKindConstant", { fg = color.orange, bg = darken_bg })
-hl("CmpItemKindFunction", { fg = color.magenta, bg = darken_bg })
-hl("CmpItemKindTypeParameter", { fg = color.green, bg = darken_bg })
+-- Set kind highlights
+hl("CmpItemKindText", { fg = color.green, bg = default_bg })
+hl("CmpItemKindSnippet", { fg = color.yellow, bg = default_bg })
+hl("CmpItemKindCopilot", { fg = color.green1, bg = default_bg })
+hl("CmpItemKindKeyword", { fg = color.purple, bg = default_bg })
+hl("CmpItemKindOperator", { fg = color.blue5, bg = default_bg })
+hl("CmpItemKindConstant", { fg = color.orange, bg = default_bg })
+hl("CmpItemKindFunction", { fg = color.magenta, bg = default_bg })
+hl("CmpItemKindTypeParameter", { fg = color.green, bg = default_bg })
